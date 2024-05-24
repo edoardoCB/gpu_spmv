7b71215ef50b53538d3773ef75f60b9895b039bf
Edoardo Coronado <eecb76@hotmail.com>
2024-05-24 17:32:54 +0200

    CHORE: added license header to bash files

--- GPU_MDE_BS0032_KAY.sh
+++ GPU_MDE_BS0032_KAY.sh
@@ -1,4 +1,44 @@
 #!/bin/sh
+#
+#            gpu_spmv
+# (SpMV product on NVIDIA's GPU)
+#
+# (C) 2019, University of Santiago de Compostela
+#
+# Author: Edoardo Coronado <eecb76@hotmail.com>
+#
+# Program: gpu_spmv
+# File: GPU_MDE_BS0032_KAY.sh
+# code dated: 12-08-2019 (dd-mm-yyyy)
+#
+#	gpu_spmv is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+////////////////////////////////////////////////////////////////
+
+echo
+echo "gpu_spmv - SpMV product on NVIDIA's GPU"
+echo "File: GPU_MDE_BS0032_KAY.sh"
+echo "(C) 2019, University of Santiago de Compostela"
+echo "Author: Edoardo Coronado"
+echo
+echo "This program comes with ABSOLUTELY NO WARRANTY."
+echo "This is free software, and you are welcome to redistribute it under"
+echo "certain conditions; see README.md and LICENSE.txt for details."
+echo
+
+
+
 #SBATCH --job-name=GPU_MDE_BS0032_KAY
 #SBATCH --partition=GpuQ
 #SBATCH --account=hpce3ic5
--- gc.sh
+++ gc.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+#
+#            gpu_spmv
+# (SpMV product on NVIDIA's GPU)
+#
+# (C) 2019, University of Santiago de Compostela
+#
+# Author: Edoardo Coronado <eecb76@hotmail.com>
+#
+# Program: gpu_spmv
+# File: gc.sh
+# code dated: 05-06-2019 (dd-mm-yyyy)
+#
+#	gpu_spmv is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+////////////////////////////////////////////////////////////////
+
+echo
+echo "gpu_spmv - SpMV product on NVIDIA's GPU"
+echo "File: gc.sh"
+echo "(C) 2019, University of Santiago de Compostela"
+echo "Author: Edoardo Coronado"
+echo
+echo "This program comes with ABSOLUTELY NO WARRANTY."
+echo "This is free software, and you are welcome to redistribute it under"
+echo "certain conditions; see README.md and LICENSE.txt for details."
+echo
 
 
 
